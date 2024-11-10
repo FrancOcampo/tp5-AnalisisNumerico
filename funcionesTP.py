ddcec3c2d2fe899c304258711a9a2fa4aa746c58
FrancOcampo <feocampo@frsf.utn.edu.ar>
2024-11-10 19:22:22 -0300

    agregado ejercicio 3

--- funcionesTP.py
+++ funcionesTP.py
@@ -8,6 +8,8 @@ m2=1
 m3=1
 
 def init(pasos):
+    et=np.zeros(pasos)
+
     p1=np.zeros((pasos,3))
     p2=np.zeros((pasos,3))
     p3=np.zeros((pasos,3))
@@ -19,7 +21,7 @@ def init(pasos):
     v3=np.zeros((pasos,3))
 
     v1[0],v2[0],v3[0]=np.array([0.5,-0.5,0.0]),np.array([-0.5,0.0,0.5]),np.array([0.0,0.5,-0.5])
-    return p1,p2,p3,v1,v2,v3
+    return p1,p2,p3,v1,v2,v3,et
 
 def calcularAceleracion(m2,m3,r1,r2,r3,G):
     r1_2=r2-r1
@@ -30,20 +32,46 @@ def calcularAceleracion(m2,m3,r1,r2,r3,G):
     aceleracion2=(G*m3*r1_3)/(distancia2**3)
     return aceleracion1+aceleracion2
 
-def graficar(p1,p2,p3,titulo_ventana='Gráfico 3D'):
-    figura = plt.figure()
-    ax = figura.add_subplot(111, projection='3d')
-    ax.plot(p1[:,0],p1[:,1],p1[:,2],color='red',label='Cuerpo 1')
-    ax.plot(p2[:,0],p2[:,1],p2[:,2],color='blue',label='Cuerpo 2')
-    ax.plot(p3[:,0],p3[:,1],p3[:,2],color='green',label='Cuerpo 3')
-    ax.scatter(p1[-1,0],p1[-1,1],p1[-1,2],color='red',s=30,label='Fin Cuerpo 1')
-    ax.scatter(p2[-1,0],p2[-1,1],p2[-1,2],color='blue',s=30,label='Fin Cuerpo 2')
-    ax.scatter(p3[-1,0],p3[-1,1],p3[-1,2],color='green',s=30,label='Fin Cuerpo 3')
+# def graficar(p1,p2,p3,titulo_ventana='Gráfico 3D'):
+#     figura = plt.figure()
+#     ax = figura.add_subplot(111, projection='3d')
+#     ax.plot(p1[:,0],p1[:,1],p1[:,2],color='red',label='Cuerpo 1')
+#     ax.plot(p2[:,0],p2[:,1],p2[:,2],color='blue',label='Cuerpo 2')
+#     ax.plot(p3[:,0],p3[:,1],p3[:,2],color='green',label='Cuerpo 3')
+#     ax.scatter(p1[-1,0],p1[-1,1],p1[-1,2],color='red',s=30,label='Fin Cuerpo 1')
+#     ax.scatter(p2[-1,0],p2[-1,1],p2[-1,2],color='blue',s=30,label='Fin Cuerpo 2')
+#     ax.scatter(p3[-1,0],p3[-1,1],p3[-1,2],color='green',s=30,label='Fin Cuerpo 3')
+#     # Establecer el título de la ventana
+#     figura.canvas.manager.set_window_title(titulo_ventana)
+#     # ax.legend()
+#     # plt.show()
+#     return figura,ax
+def graficar(p1,p2,p3,energia_total, titulo_ventana='Gráfico 3D'):
+    figura = plt.figure(figsize=(12, 6))
+
+    # Subplot 1: Gráfico 3D de las posiciones
+    ax1 = figura.add_subplot(121, projection='3d')
+    ax1.plot(p1[:,0], p1[:,1], p1[:,2], color='red', label='Cuerpo 1')
+    ax1.plot(p2[:,0], p2[:,1], p2[:,2], color='blue', label='Cuerpo 2')
+    ax1.plot(p3[:,0], p3[:,1], p3[:,2], color='green', label='Cuerpo 3')
+    ax1.scatter(p1[-1,0], p1[-1,1], p1[-1,2], color='red', s=30, label='Fin Cuerpo 1')
+    ax1.scatter(p2[-1,0], p2[-1,1], p2[-1,2], color='blue', s=30, label='Fin Cuerpo 2')
+    ax1.scatter(p3[-1,0], p3[-1,1], p3[-1,2], color='green', s=30, label='Fin Cuerpo 3')
+    ax1.set_title('Trayectorias de los cuerpos')
+    ax1.legend()
+
+    # Subplot 2: Gráfico de la energía total
+    ax2 = figura.add_subplot(122)
+    ax2.plot(energia_total, color='purple', label='Energía Total')
+    ax2.set_title('Energía Total del Sistema')
+    ax2.set_xlabel('Tiempo')
+    ax2.set_ylabel('Energía')
+    ax2.legend()
+
     # Establecer el título de la ventana
     figura.canvas.manager.set_window_title(titulo_ventana)
-    # ax.legend()
-    # plt.show()
-    return figura,ax
+
+    return figura, (ax1, ax2)
 
 #1-A-EULER----------------------------------------------------------------------------------------------------
 def calcularEuler(c1, c2, Delta_t):
@@ -59,10 +87,10 @@ def ejecutarEuler(G,pasos,delta_t):
     # delta_t=0.001
     titulo_ventana='Euler'
 
-    p1,p2,p3,v1,v2,v3=init(pasos)
+    p1,p2,p3,v1,v2,v3,et=init(pasos)
 
     for i in range(pasos-1):
-        
+        et[i]=calcularEnergia_i(p1[i],p2[i],p3[i],v1[i],v2[i],v3[i],G)
         p1[i+1]=calcularEuler(p1[i],v1[i],delta_t)
         p2[i+1]=calcularEuler(p2[i],v2[i],delta_t)
         p3[i+1]=calcularEuler(p3[i],v3[i],delta_t)
@@ -71,14 +99,14 @@ def ejecutarEuler(G,pasos,delta_t):
         v2[i+1]=calcularEuler(v2[i],calcularAceleracion(m1,m3,p2[i],p1[i],p3[i],G),delta_t)
         v3[i+1]=calcularEuler(v3[i],calcularAceleracion(m1,m2,p3[i],p1[i],p2[i],G),delta_t)
 
-    return graficar(p1,p2,p3,titulo_ventana)
+    return graficar(p1,p2,p3,et,titulo_ventana)
 
 def ejecutarEuler_heunn(G,pasos,delta_t):
     titulo_ventana='Euler-Heunn'
-    p1,p2,p3,v1,v2,v3=init(pasos)
+    p1,p2,p3,v1,v2,v3,et=init(pasos)
 
     for i in range(pasos-1):
-        
+        et[i]=calcularEnergia_i(p1[i],p2[i],p3[i],v1[i],v2[i],v3[i],G)
         p1_2=calcularEuler(p1[i],v1[i],delta_t)
         p2_2=calcularEuler(p2[i],v2[i],delta_t)
         p3_2=calcularEuler(p3[i],v3[i],delta_t)
@@ -91,14 +119,16 @@ def ejecutarEuler_heunn(G,pasos,delta_t):
         p2[i+1]=calcularEuler_Heunn(p2[i],v2[i],v2[i+1],delta_t)
         p3[i+1]=calcularEuler_Heunn(p3[i],v3[i],v3[i+1],delta_t)
 
-    return graficar(p1,p2,p3,titulo_ventana)
+    return graficar(p1,p2,p3,et,titulo_ventana)
 #Fin__1-A-EULER----------------------------------------------------------------------------------------------------
 
 def ejecutarRK4(G,pasos,delta_t):
     titulo_ventana='RK4'
-    p1,p2,p3,v1,v2,v3=init(pasos)
-
+    p1,p2,p3,v1,v2,v3,et=init(pasos)
+    
     for i in range(pasos-1):
+        et[i]=calcularEnergia_i(p1[i],p2[i],p3[i],v1[i],v2[i],v3[i],G)
+
         a1=calcularAceleracion(m2,m3,p1[i],p2[i],p3[i],G)
         a2=calcularAceleracion(m1,m3,p2[i],p1[i],p3[i],G)
         a3=calcularAceleracion(m1,m2,p3[i],p1[i],p2[i],G)
@@ -127,5 +157,24 @@ def ejecutarRK4(G,pasos,delta_t):
         p2[i+1]=p2[i]+(delta_t/6)*(k1_2+2*k2_2+2*k3_2+k4_2)
         p3[i+1]=p3[i]+(delta_t/6)*(k1_3+2*k2_3+2*k3_3+k4_3)
 
-    return graficar(p1,p2,p3,titulo_ventana)
+    return graficar(p1,p2,p3,et,titulo_ventana)
+    
+def calcularECinetica(m_i,v_i):
+    return (m_i/2)*np.linalg.norm(v_i)**2
+
+def calcularEPotencial(m1,m2,m3,p1,p2,p3,G):
+    r1_2=p2-p1
+    r1_3=p3-p1
+    distancia1=np.linalg.norm(r1_2)
+    distancia2=np.linalg.norm(r1_3)
+    return -G*m1*m2/distancia1-G*m1*m3/distancia2 #suma de las energias potenciales de los cuerpos
+
+def calcularEnergiaTotal(ep,ec):
+    return ep+ec
+
+def calcularEnergia_i(p1_i,p2_i,p3_i,v1_i,v2_i,v3_i,G):
+    ep=calcularEPotencial(m1,m2,m3,p1_i,p2_i,p3_i,G)
+    ec=calcularECinetica(m1,v1_i)+calcularECinetica(m2,v2_i)+calcularECinetica(m3,v3_i)
+    et=calcularEnergiaTotal(ep,ec)
+    return et
     
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import funcionesTP as func
 import matplotlib.pyplot as plt
 
-fig1,ax1=func.ejecutarEuler(2.2,5000,0.001)
+fig1,(ax1,ax11)=func.ejecutarEuler(2.2,5000,0.001)
 
-fig2,ax2=func.ejecutarEuler_heunn(2.2,5000,0.001)
+fig2,(ax2,ax22)=func.ejecutarEuler_heunn(2.2,5000,0.001)
 
-fig3,ax3=func.ejecutarRK4(2.2,5000,0.001)
+fig3,(ax3,ax33)=func.ejecutarRK4(2.2,5000,0.001)
 
 plt.show()
